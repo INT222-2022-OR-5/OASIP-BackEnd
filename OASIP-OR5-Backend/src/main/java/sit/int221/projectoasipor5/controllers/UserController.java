fd13370667b3479cc6cbb78c62b99c453486cf4a
Wongsakorn Saengsurasak <68773278+highgmy6901@users.noreply.github.com>
2022-08-23 23:28:08 +0700

    Add files via upload

---
+++ OASIP-OR5-Backend/src/main/java/sit/int221/projectoasipor5/controllers/UserController.java
@@ -0,0 +1,47 @@
+package sit.int221.projectoasipor5.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import sit.int221.projectoasipor5.dto.UserAddDTO;
+import sit.int221.projectoasipor5.dto.UserDTO;
+import sit.int221.projectoasipor5.entities.User;
+import sit.int221.projectoasipor5.services.UserService;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    //Get all user
+    @GetMapping
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUser();
+    }
+
+    //Get user with id
+    @GetMapping("/{id}")
+    public UserDTO getUserById(@PathVariable Integer id){
+        return userService.getUserById(id);
+    }
+
+    //Update user with id
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@Validated @RequestBody UserAddDTO newUser) {
+        return userService.createUser(newUser);
+    }
+
+    //Delete user with id
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userService.deleteById(id);
+    }
+
+
+}
