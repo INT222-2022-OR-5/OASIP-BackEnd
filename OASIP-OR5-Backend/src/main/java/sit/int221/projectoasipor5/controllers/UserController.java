696a7820733784f1c60e1bf4281bff2381b6bb79
Wongsakorn <wong.saengsurasak@gmail.com>
2022-08-26 20:45:36 +0700

    Update [edit user]

--- OASIP-OR5-Backend/src/main/java/sit/int221/projectoasipor5/controllers/UserController.java
+++ OASIP-OR5-Backend/src/main/java/sit/int221/projectoasipor5/controllers/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import sit.int221.projectoasipor5.dto.UserAddDTO;
 import sit.int221.projectoasipor5.dto.UserDTO;
+import sit.int221.projectoasipor5.dto.UserUpdateDTO;
 import sit.int221.projectoasipor5.entities.User;
 import sit.int221.projectoasipor5.services.UserService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -30,7 +32,7 @@ public class UserController {
         return userService.getUserById(id);
     }
 
-    //Update user with id
+    //Create user with id
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@Validated @RequestBody UserAddDTO newUser) {
@@ -43,5 +45,10 @@ public class UserController {
         userService.deleteById(id);
     }
 
+    //Update user with id
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Integer id,@RequestBody UserUpdateDTO updateUser){
+        return userService.updateUser(updateUser, id);
+    }
 
 }
--- OASIP-OR5-Backend/src/main/java/sit/int221/projectoasipor5/services/UserService.java
+++ OASIP-OR5-Backend/src/main/java/sit/int221/projectoasipor5/services/UserService.java
@@ -10,6 +10,7 @@ import sit.int221.projectoasipor5.Utils.ListMapper;
 import sit.int221.projectoasipor5.Utils.Role;
 import sit.int221.projectoasipor5.dto.UserAddDTO;
 import sit.int221.projectoasipor5.dto.UserDTO;
+import sit.int221.projectoasipor5.dto.UserUpdateDTO;
 import sit.int221.projectoasipor5.entities.User;
 import sit.int221.projectoasipor5.repositories.UserRepository;
 
@@ -55,7 +56,40 @@ public class UserService {
         repository.deleteById(id);
     }
 
-    private User mapUser(User existingUser , User updateUser){
+    public User updateUser(UserUpdateDTO updateUser, Integer id) {
+        updateUser.setName(updateUser.getName().trim());
+        updateUser.setEmail(updateUser.getEmail().trim());
+        User user = repository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, id + " does not exist please find new id if exist."));
+
+        if (checkIsNotUnique(updateUser, user)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "name or email must unique");
+        }
+        user = repository.findById(id).map(
+                o -> mapUser(o, updateUser)).orElseGet(() -> {
+            updateUser.setId(id);
+            return modelMapper.map(updateUser, User.class);
+        });
+        return repository.saveAndFlush(user);
+    }
+
+    private boolean checkIsNotUnique(UserUpdateDTO updateUser, User user){
+        List<User> users = repository.findAll();
+        boolean[] isNotUnique = {false};
+        users.forEach(
+                e -> {
+                    if (user.getId() != e.getId()) {
+                        if (updateUser.getName().toLowerCase().equals(e.getName().toLowerCase()) ||
+                                updateUser.getEmail().toLowerCase().equals(e.getEmail().toLowerCase())){
+                            isNotUnique[0] = true;
+                        }
+                    }
+                }
+        );
+        return isNotUnique[0];
+    }
+
+    private User mapUser(User existingUser , UserUpdateDTO updateUser){
         existingUser.setName(updateUser.getName());
         existingUser.setEmail(updateUser.getEmail());
         existingUser.setRole(updateUser.getRole());
